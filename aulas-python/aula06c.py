49593f30a61433d0a0b13268ec25c0aa9232d53f
israellefraim <israell.efraim@gmail.com>
2025-03-26 09:48:33 -0300

    Tipos de Dados

--- aulas-python/aula06c.py
+++ aulas-python/aula06c.py
@@ -1,7 +1,7 @@
 n = str(input('Digite um valor: '))
 print(n)
 
-n1 = float(input('\nDigite um valor: '))  # float = número real
+n1 = float(input('\nDigite um valor: '))
 print(n1)
 
 n2 = bool(input('\nDigite um valor: '))  # operador lógico = True or False
