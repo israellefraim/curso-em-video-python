259f9efad5c869bedb4606915014c29ec06ace1b
israellefraim <israell.efraim@gmail.com>
2025-03-26 09:54:17 -0300

    Número aleatório com Random

--- aulas-python/aula08c.py
+++ aulas-python/aula08c.py
@@ -1,6 +1,6 @@
 import random
 
-num = random.randint(1, 10)  # número inteiro aleatório de 1 a 10
-print(num)
+num1 = random.randint(1, 10)  # número inteiro aleatório de 1 a 10
+print(num1)
 num2 = random.random()  # número aleatório de 0 a 1
 print(num2)
