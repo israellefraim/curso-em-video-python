ada4c909c34fb526404c3ea804c11c7a8c589829
israellefraim <israell.efraim@gmail.com>
2025-03-26 10:16:55 -0300

    Métodos de Transformação de String

--- aulas-python/aula09c.py
+++ aulas-python/aula09c.py
@@ -4,7 +4,7 @@ print(frase.upper())  # Faz todas as letras ficarem maiúsculas
 print(frase.lower())  # Faz todas as letras ficarem minúsculas
 print(frase.capitalize())  # Apenas a primeira letra da variável fica maiúscula
 print(frase.title())  # Todas as primeiras letras das palavras ficam maiúsculas
-print(frase.upper().count('O'))  # Combinação de duas funções diferentes
+print(frase.upper().count('O'))  # Deixa tudo em caixa alta e dps conta a quantidade de determinado caractere
 print(frase.lower().find('vídeo'))  # Após deixar tudo em mínusculo, ele diz a posição da palavra
 frase = frase.replace('Python', 'Android')  # Substituindo de maneira permanente um termo por outro
 print(frase)
