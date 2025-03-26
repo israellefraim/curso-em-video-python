1aa40b1c9d5ef2c1b1410c06ec6a19669c9d230e
israellefraim <israell.efraim@gmail.com>
2025-03-26 09:42:59 -0300

    Valida tipo Dado

---
+++ aulas-python/aula06d.py
@@ -0,0 +1,11 @@
+n = input('Digite algo: ')
+print(n.isnumeric())  # validar se é um número com True or False
+
+n1 = input('\nDigite algo: ')
+print(n1.isalpha())  # validar se é alfabético
+
+n2 = input('\nDigite algo: ')
+print(n2.isalnum())  # validar se é numérico ou/e alfabético
+
+n3 = input('\nDigite algo: ')
+print(n2.is_integer())  # verifica se a parte decimal é zero e se o número equivale a um inteiro
