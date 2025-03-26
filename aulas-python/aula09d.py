0db28f223640346a46a5425e29f09e5429a1704f
israellefraim <israell.efraim@gmail.com>
2025-03-26 10:18:33 -0300

    Métodos Remove Espaço Vazio

---
+++ aulas-python/aula09d.py
@@ -0,0 +1,4 @@
+frase = "   Aprenda Python  "
+print(frase.strip())  # Remove os espaços inúteis do ínicio e do final
+print(frase.rstrip())  # Remove apenas os espaços inúteis da direita por conta do 'r' antes da função strip
+print(frase.lstrip())  # Remove apenas os espaços inúteis da esquerda
