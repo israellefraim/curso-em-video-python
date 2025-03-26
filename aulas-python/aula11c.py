a1a5cb08db9871dd27301ae6bfe21b766ee1d181
israellefraim <israell.efraim@gmail.com>
2025-03-26 10:26:03 -0300

    Cores no Terminal com Dicionário

---
+++ aulas-python/aula11c.py
@@ -0,0 +1,7 @@
+nome = 'Israel'
+cores = {'limpa': '\033[m',
+         'azul': '\033[94m',
+         'vermelho': '\033[91m',
+         'pretoebranco': '\033[7;97m'}
+
+print(f'Olá. Muito prazer em te conhecer {cores['azul']}{nome}{cores['limpa']}.')
