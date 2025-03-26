3330089ffc20921c6fd12d2fe31803988254e1d1
israellefraim <israell.efraim@gmail.com>
2025-03-26 09:56:34 -0300

    Fatiamento

---
+++ aulas-python/aula09a.py
@@ -0,0 +1,8 @@
+frase = "Curso em Vídeo Python"
+print(frase[0])  # Caractere do índice indicado
+print(frase[0:13])  # Caracteres do índice 9 ao 12
+print(frase[9:21])
+print(frase[0:21:2])  # Caracteres do índice 9 ao 12 pulando a cada 2 casas
+print(frase[:5])  # Início da variável até o índice 4
+print(frase[15:])  # Ínicio no índice 15 até o final da variável
+print(frase[9::3])  # Ínicio no índice 9 até o final da variável pulando a cada 3 casas
