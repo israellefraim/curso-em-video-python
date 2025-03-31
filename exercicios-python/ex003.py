3d3f2e8622dcec423cf1190e8803caeca47c6a4a
israellefraim <israell.efraim@gmail.com>
2025-03-31 11:58:07 -0300

    Somando dois números

---
+++ exercicios-python/ex003.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite o \033[1;94m1º\033[m valor: '))
+n2 = int(input('Digite o \033[1;94m2º\033[m valor: '))
+result = n1 + n2
+print(f'\nA soma entre \033[32m{n1}\033[m e \033[32m{n2}\033[m é igual a: \033[1;96m{result}')
