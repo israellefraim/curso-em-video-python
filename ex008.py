164d9c9c73caf5db214c561e0bc89d3dc1760cf1
israellefraim <israell.efraim@gmail.com>
2025-04-08 10:02:02 -0300

    Conversos de Medidas

---
+++ ex008.py
@@ -0,0 +1,4 @@
+metro = float(input('Digite uma quantidade de \033[94mmetros\033[m: '))
+cent = metro * 100
+mili = cent * 10
+print(f'\nO valor em centímetros é de \033[95m{cent:.0f}\033[m, e em milímetros \033[96m{mili:.0f}\033[m.')
