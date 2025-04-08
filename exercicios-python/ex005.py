93f0bc27cd4af0826eb9259df2d5d84cec19b637
israellefraim <israell.efraim@gmail.com>
2025-04-08 10:00:25 -0300

    Antecessor e Sucessor

---
+++ exercicios-python/ex005.py
@@ -0,0 +1,4 @@
+num1 = int(input('Digite um valor inteiro: \033[1;4;97m'))
+su = num1 + 1
+ant = num1 - 1
+print(f'\033[m\nO antercessor é \033[1;94m{ant}\033[m, e o sucessor \033[1;94m{su}\033[m.')
