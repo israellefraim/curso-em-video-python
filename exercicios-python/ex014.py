b556993af75f8ccd0ad26ba351bb7696ad1da923
israellefraim <israell.efraim@gmail.com>
2025-04-08 10:12:17 -0300

    Conversor de Temperaturas

---
+++ exercicios-python/ex014.py
@@ -0,0 +1,3 @@
+cel = float(input('Digite a \033[4;91mtemperatura em celsius\033[m: '))
+far = (cel * 1.8) + 32
+print(f'Esta temperatura em \033[4;94mgraus Fahrenheit\033[m é de \033[1;97m{far}ºF\033[m')
