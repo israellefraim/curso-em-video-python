4d7627c2b85b1e2b4185fbbecf89e65b6c886a99
israellefraim <israell.efraim@gmail.com>
2025-04-08 10:17:42 -0300

    Seno, Cosseno e Tangente

---
+++ exercicios-python/ex018.py
@@ -0,0 +1,9 @@
+from math import sin, cos, tan, radians
+ang = int(input('Digite o ângulo que você deseja: '))
+sen = sin(radians(ang))
+cos = cos(radians(ang))
+tan = tan(radians(ang))
+print(
+    f'\033[96mSeno\033[m de {ang}º é \033[1;96m{sen:.4f} '
+    f'\033[m \n\033[94mCosseno\033[m de {ang}º é \033[1;94m{cos:.4f}\033[m '
+    f'\n\033[95mTangente\033[m de {ang}º é \033[1;95m{tan:.4f}\033[m')
