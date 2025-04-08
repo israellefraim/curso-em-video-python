257bd7d4137572392cbf963d952dc7dd72656d94
israellefraim <israell.efraim@gmail.com>
2025-04-08 10:08:01 -0300

    Conversor de Moedas

---
+++ exercicios-python/ex010.py
@@ -0,0 +1,3 @@
+quant = float(input('Digite a \033[97mquantidade de dinheiro\033[m situada na sua carteira: R$'))
+dol = quant / 3.27
+print(f'\nO valor em \033[97mdólares\033[m é de \033[32m${dol:.2f}\033[m')
