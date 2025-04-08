cd59e6410be9a56385ac1db7c6e7d37c5f3603d0
israellefraim <israell.efraim@gmail.com>
2025-04-08 10:07:30 -0300

    Tabuada

---
+++ exercicios-python/ex009.py
@@ -0,0 +1,13 @@
+num = int(input('Digite um número: '))
+a = num * 1
+b = num * 2
+c = num * 3
+d = num * 4
+e = num * 5
+f = num * 6
+g = num * 7
+h = num * 8
+i = num * 9
+j = num * 10
+print(
+    f"\nA tabuada de {num} é:\n\033[94m------------\n1 x {num} = {a}\n2 x {num} = {b}\n3 x {num} = {c}\n4 x {num} = {d}\n5 x {num} = {e}\n6 x {num} = {f}\n7 x {num} = {g}\n8 x {num} = {h}\n9 x {num} = {i}\n10 x {num} = {j}\n------------")
