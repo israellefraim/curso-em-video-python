53617efe6b4a66f85e951481be88d275eb0e008b
israellefraim <israell.efraim@gmail.com>
2025-04-22 11:03:17 -0300

    Primeiro e último nome de uma de uma pessoa

--- exercicios-python/ex027.py
+++ exercicios-python/ex027.py
@@ -1,4 +1,4 @@
 name = str(input("Digite o seu \033[1;95mnome completo\033[m: ")).title().strip().split()
 print("\n\033[7mMuito prazer em te conhecer!\033[m")
-print(f"Seu primeiro nome é \033[32m{name[0]}\033[m")
+print(f"Seu primeiro nome é \033[32m{name[0]}\033[m") 
 print(f"Seu último nome é \033[32m{name[-1]}\033[m\033[m")  # Os índices negativos são usados para acessar elementos a partir do final da lista. O -1 se refere ao último elemento da lista, - 2 ao penúltimo, etc.
