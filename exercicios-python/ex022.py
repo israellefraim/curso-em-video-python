bed7fd59eac412a3321f131353244d52651b8ac8
israellefraim <israell.efraim@gmail.com>
2025-04-22 10:11:46 -0300

    Analisador de Textos

--- exercicios-python/ex022.py
+++ exercicios-python/ex022.py
@@ -2,7 +2,7 @@ from time import sleep
 nome = str(input("Escreva seu \033[1mnome completo\033[m: ")).strip()  # strip para tirar os espaços em branco desnecessários do início e do final
 print("\n\033[1;95mAnalisando seu nome...\033[m")
 sleep(3)
-print('=-' * 30)
+print('=-' * 30) 
 print(f"\033[97mSeu nome em letras maiúsculas é {nome.upper()}.")
 print(f"Seu nome em letras minúsculas é {nome.lower()}.")
 print(f"Seu nome tem {len(''.join(nome.split()))} letras.")  # Primeiro separa as palavras para deixá-las sem espaços, dps junta novamente com join, e conta
