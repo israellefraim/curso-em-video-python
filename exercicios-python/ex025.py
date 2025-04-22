a03a0f32822fa9d531ecc77ebb22b91be62fd952
israellefraim <israell.efraim@gmail.com>
2025-04-22 10:25:49 -0300

    Procurando uma string dentro de outra

--- exercicios-python/ex025.py
+++ exercicios-python/ex025.py
@@ -1,2 +1,2 @@
 name = str(input("Digite seu \033[96mnome completo:\033[m ")).strip().upper().split()
-print(f"Seu nome tem Silva? \033[95m{"SILVA" in name}\033[m")
+print(f"Seu nome tem Silva? \033[95m{"SILVA" in name}\033[m") 
