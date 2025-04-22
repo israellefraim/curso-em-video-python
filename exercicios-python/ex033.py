c82e3088ccf97b95a8f12d134bd68a70b8be04c3
israellefraim <israell.efraim@gmail.com>
2025-04-22 11:37:39 -0300

    Maior e menor valor

---
+++ exercicios-python/ex033.py
@@ -0,0 +1,5 @@
+num_1 = int(input("Insira o 1º número inteiro: "))
+num_2 = int(input("Insira o 2º número inteiro: "))
+num_3 = int(input("Insira o 3º número inteiro: "))
+
+print(f"\nO \033[1;4mmaior\033[m número é: \033[1;94m{max(num_1, num_2, num_3)}\033[m\nO \033[1;4mmenor\033[m número é:\033[m \033[1;96m{min(num_1, num_2, num_3)}\033[m")
