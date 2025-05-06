99a3a16bb8febe84307336442c9fe97081e3c81b
Israel Efraim <israell.efraim@gmail.com>
2025-05-06 11:43:41 -0300

    Adiciona comentários

--- aulas-python/aula09a.py
+++ aulas-python/aula09a.py
@@ -1,8 +1,9 @@
 frase = "Curso em Vídeo Python"
 print(frase[0])  # Caractere do índice indicado
-print(frase[0:13])  # Caracteres do índice 9 ao 12
-print(frase[9:21])
-print(frase[0:21:2])  # Caracteres do índice 9 ao 12 pulando a cada 2 casas
-print(frase[:5])  # Início da variável até o índice 4
-print(frase[15:])  # Ínicio no índice 15 até o final da variável
-print(frase[9::3])  # Ínicio no índice 9 até o final da variável pulando a cada 3 casas
+print(frase[0:13])  # Caracteres do índice 0 ao 12
+print(frase[9:21])  # Caracteres do índice 9 ao 12
+print(frase[0:21:2])  # Caracteres do índice 0 ao 21 pulando a cada 2 casas
+print(frase[:5])  # Caracteres do índice 0 ao 5
+print(frase[15:])  # Início no índice 15 até o final da variável
+print(frase[9::3])  # Início no índice 9 até o final da variável pulando a cada 3 casas
+print(frase[::4])  # Início no índice 0 até o final da variável pulado a cada 4 casas
