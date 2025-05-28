73dd4e28ad2dc29632ee7babbc37ec6fc2c3774d
israellefraim <israell.efraim@gmail.com>
2025-05-28 10:46:56 -0300

    Adiciona descrição do exercício

--- exercicios-python/ex062.py
+++ exercicios-python/ex062.py
@@ -1,3 +1,9 @@
+'''
+Melhore o exercício 61, perguntando para o usuário
+se ele quer mostrar mais alguns termos
+O programa encerra quando ele disser que quer mostrar "0 termos"
+'''
+
 quantidade_termos = int(input("Insira a quantidade de termos na PA: "))
 if quantidade_termos < 1:
     exit()
