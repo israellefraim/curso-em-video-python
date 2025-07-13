d253d7875eaf73b6ac2bf9f932ffaa97992d9dff
israellefraim <israell.efraim@gmail.com>
2025-07-13 16:45:10 -0300

    Melhora ex057

--- exercicios-python/ex057.py
+++ exercicios-python/ex057.py
@@ -1,4 +1,5 @@
 sexo = "w"
 
-while sexo not in ['M', 'F']:
-    sexo = str(input('Insira seu sexo (M/F): ')).strip().upper()
+while sexo not in ['M', 'F']:  # Se o texto digitado não for M nem F...
+    sexo = str(input('Insira seu sexo (M/F): ')).strip().upper()[0]  # Pega somente a primeira letra da resposta
+print(f'Sexo {sexo} registrado com sucesso!')
