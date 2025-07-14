e6bdd760b24fe0c1bce082de1e32f310da6601c6
israellefraim <israell.efraim@gmail.com>
2025-07-14 20:34:07 -0300

    Adiciona enunciado do ex051

--- exercicios-python/ex051.py
+++ exercicios-python/ex051.py
@@ -1,3 +1,9 @@
+''' 
+Desenvolva um programa que leia o primeiro termo e a razão
+de uma PA (Progressão Aritmética).
+No final, mostre os 10 primeiros termos dessa progressão.
+'''
+
 n1 = int(input('Insira o primeiro termo da PA: '))
 r = int(input('Insira a razão da PA: '))
 fim = n1 + (r * 10)
