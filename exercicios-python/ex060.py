13ed85dde9adc0e650053ae66939bcfea976a20e
israellefraim <israell.efraim@gmail.com>
2025-07-14 20:26:43 -0300

    Aperfeiçoa ex060

--- exercicios-python/ex060.py
+++ exercicios-python/ex060.py
@@ -1,13 +1,25 @@
-#import math
+''' 
+Faça um programa que leia um número qualquer e mostre
+o seu fatorial.
 
-num = int(input('Insira um número: '))
+Ex.: 5! = 5 * 4 * 3 * 2 * 1 = 120
+'''
+
+#import math
 #print(math.factorial(numero))
 
-total = 1
+'''
+for c in range(numero, 0, -1):
+    total *= numero
+    print(f'{numero} x ' if numero != 1 else f'{numero} = {total}', end='')
+    numero -= 1
+'''
 
-while (num >= 1):
-    total = total * num
-    num = num - 1
-    
-print(total)
-    
+total = 1
+numero = int(input('Insira um número: '))
+
+print(f'{numero}! = ', end='')
+while (numero >= 1):
+    total *= numero
+    print(f'{numero} x ' if numero != 1 else f'{numero} = {total}', end='')
+    numero -= 1
