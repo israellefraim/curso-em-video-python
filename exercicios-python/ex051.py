2129ff31e0776971e82aa024361b22e7914ba283
israellefraim <israell.efraim@gmail.com>
2025-07-14 21:06:50 -0300

    Adiciona descrição do ex051

--- exercicios-python/ex051.py
+++ exercicios-python/ex051.py
@@ -4,9 +4,9 @@ de uma PA (Progressão Aritmética).
 No final, mostre os 10 primeiros termos dessa progressão.
 '''
 
-n1 = int(input('Insira o primeiro termo da PA: '))
-r = int(input('Insira a razão da PA: '))
-fim = n1 + (r * 10)
-for c in range(n1, fim, r):
+primeiro_termo = int(input('Insira o primeiro termo da PA: '))
+razao = int(input('Insira a razao da PA: '))
+fim = primeiro_termo + (razao * 10)
+for c in range(primeiro_termo, fim, razao):
     print(f'{c}', end=' -> ')
 print('ACABOU')
