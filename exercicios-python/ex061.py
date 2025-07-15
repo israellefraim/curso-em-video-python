4f987098ec8279e7db3178a6e703e3a7a701f70e
israellefraim <israell.efraim@gmail.com>
2025-07-14 21:34:14 -0300

    Aperfeiçoa ex061

--- exercicios-python/ex061.py
+++ exercicios-python/ex061.py
@@ -1,3 +1,9 @@
+'''
+Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA,
+mostrando os 10 primeiros termos da progressão usando
+a estrutura while
+'''
+
 primeiro_termo = int(input('Insira o primeiro termo da PA: '))
 razao = int(input('Insira a razão da PA: '))
 fim = primeiro_termo + (razao * 9)
@@ -5,8 +11,7 @@ fim = primeiro_termo + (razao * 9)
 print(primeiro_termo, end = " -> ")
 
 while (primeiro_termo < fim):
-    print(primeiro_termo + razao, end = " -> ")
-    primeiro_termo = primeiro_termo + razao
-
+    primeiro_termo += razao
+    print(primeiro_termo, end = " -> ")
 
 print('ACABOU')
