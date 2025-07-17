1e3596dca400f99d47c415ab09ccb6a5d2230e24
israellefraim <israell.efraim@gmail.com>
2025-07-17 13:27:48 -0300

    Realiza ex064

--- exercicios-python/ex064.py
+++ exercicios-python/ex064.py
@@ -5,3 +5,13 @@ condição de parada.No final, mostre quantos números foram digitado
 e qual foi a soma entre eles (desconsiderando o flag).
 """
 
+numero = somador = contador = 0
+
+numero = int(input('Digite um número (999 para parar): '))
+
+while (numero != 999):
+    somador += numero
+    contador += 1 
+    numero = int(input('Digite um número (999 para parar): '))
+        
+print(f'\nVocê digitou {contador} números e a soma entre eles foi {somador}.')
