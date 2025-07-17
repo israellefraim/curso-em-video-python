04ba3036c8d072020157092a1dc233d8a3ddde07
israellefraim <israell.efraim@gmail.com>
2025-07-16 23:00:16 -0300

    Altera código

--- exercicios-python/ex063.py
+++ exercicios-python/ex063.py
@@ -1,21 +1,22 @@
-'''
+"""
 Escreva um programa que leia um número n inteiro qualquer
 e mostre na tela os primeiros n elementos e uma sequência de Fibonacci
 
-1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584 (...)
-'''
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584 (...)
+"""
 
-tamanho = int(input('Tamanho da sequência de finbonacci: '))
-c1 = 1
-c2 = 1
-valor = 0
-cont = 2
-print(f'{c1} {c2} ', end="")
+quantidade_termos = int(input('Quantidade de elementos da sequência: '))
 
-while (cont < tamanho):
-    valor = c1 + c2
-    print(valor, end=" ")
-    c1 = c2
-    c2 = valor
-    cont += 1
-    
+termo1 = 0
+termo2 = 1
+termo3 = 0
+contador = 2
+
+while (contador <= quantidade_termos):   
+    print(f'{termo3} -> ' if termo3 > 0 else '0 -> 1 -> ', end='')
+    termo3 = termo1 + termo2 
+    termo1 = termo2
+    termo2 = termo3
+    contador += 1
+
+print('FIM')
