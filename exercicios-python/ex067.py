cc2f28f9aa8a0f0d1a29e38991aab42b6696b241
israellefraim <israell.efraim@gmail.com>
2025-08-01 11:23:39 -0300

    Realizada exercício 67

---
+++ exercicios-python/ex067.py
@@ -0,0 +1,15 @@
+''' 
+Faça um programa que mostre a tabuada de vários números,
+um de cada vez, para cada valor digitado pelo usuário.
+O programa será interrompido quando o número solicitado for negativo
+'''
+
+while True:
+    numero = int(input('Digite o número de uma tabuada que deseja: '))
+    if numero < 0: break
+    for c in range(0, 11):
+        if c == 0: print(f"\033[1m{'=-'*2}TABUADA DO {numero}{'-='*2}\033[m")
+        print(f'{numero} x {c} = {numero * c}')
+    print('\033[1m=-=-=-=-=-=-=-=-=--=\033[m')
+    
+print('\nPrograma encerrado.')
