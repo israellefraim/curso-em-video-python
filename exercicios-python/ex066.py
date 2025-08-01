dda68ff55a8c64ed382ba681ebbf8e19902dd7f0
Israel Efraim <israell.efraim@gmail.com>
2025-08-01 10:54:29 -0300

    Realiza exercício 66

--- exercicios-python/ex066.py
+++ exercicios-python/ex066.py
@@ -3,5 +3,15 @@ Crie um programa que leia vários números inteiros pelo teclado.
 O programa só vai parar quando o usuário digitar 999,
 que é a condição de parada (flag).
 No final, mostre quantos números foram digitados
-e qual foi a acc entre eles (desconsiderando o flag)
+e qual foi a soma entre eles (desconsiderando o flag)
 '''
+
+numero = quantidade = soma = 0
+
+while True:
+  numero = int(input('Digite um número inteiro: '))
+  if numero == 999: break
+  quantidade += 1
+  soma += numero
+  
+print(f'\nQuantidade de número digitados: {quantidade}\nSoma dos números: {soma')
