573dd96cb892c92bb317f1f7f3a9701539a1fa9a
israellefraim <israell.efraim@gmail.com>
2026-01-03 01:12:52 -0300

    Altera programa para ser pseudocode

--- exercicios-python/ex071.py
+++ exercicios-python/ex071.py
@@ -1,15 +1,19 @@
-'''
-Crie um programa que simule o funcionamento de um caixa eletrônico. 
-No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) 
+"""
+Crie um programa que simule o funcionamento de um caixa eletrônico.
+No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro)
 e o programa vai informar quantas cédulas de cada valor serão entregues.
-OBS.: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
-'''
+OBS.: considere que o caixa possui cédulas de R$ 50, R$ 20, R$ 10 e R$ 1.
+"""
 
+# Criar uma array de valores para representar as células
+valor_cedula = [50, 20, 10, 1]
+
+# Perguntar ao usuário o valor a ser sacado e atribui esse valor a variável
 valor_sacado = int(input("Insira um valor a ser sacado: R$"))
 total_valor = valor_sacado
-valor_cedula = [50, 20, 10, 1]
-i = 0
 
+# Criar laço de repetição para definir quantas cédulas serão dadas
+i = 0
 while valor_sacado > 0:
     quantidade_cedulas = valor_sacado // valor_cedula[i]
     if quantidade_cedulas > 0:
